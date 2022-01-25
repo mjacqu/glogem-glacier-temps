ae982c3319596d6d4e240bb2c49260419c4fc617
Mylene Jacquemart <mjacqu@gmail.com>
2022-01-25 13:00:13 +0100

    added function to create csv from multiple tabs in one sheet

--- googlesheet2csv.py
+++ googlesheet2csv.py
@@ -1,24 +1,36 @@
 import pandas as pd
 import os
 
-
-def gs2csv(url, path, fname):
+#generate csv from individual Google Sheet:
+def gs2csv_singlesheet(url, path, fname):
     url_1 = url.replace('/edit#gid=','/export?format=csv&gid=')
     df = pd.read_csv(url_1)
     df.to_csv(os.path.join(path,fname), index=False)
 
+#generate csv from multiple sheets (tabs) in one Google Sheet:
+def gs2csv_multisheet(sheetid, sheetname, path, fname):
+    url = f"https://docs.google.com/spreadsheets/d/{sheetid}/gviz/tq?tqx=out:csv&sheet={sheetname}"
+    df = pd.read_csv(url)
+    df.to_csv(os.path.join(path,fname), index=False)
+
 path = '/Users/mistral/Documents/ETHZ/Science/PROGRESS/data'
+sheet_id = '1zxn_8nitemCcFP5D-pGMRHwt6UYowfWnPx_Mq3gXVo4'
+sheet_names = ['studies', 'measurement_info', 'data']
+fnames = ['01_studies.csv', '02_measurement_info.csv', '03_temperatures.csv']
+
+[gs2csv_multisheet(sheet_id, sn, path, fn) for sn, fn in zip(sheet_names, fnames)]
+
 #studies
-fname = '01_studies.csv'
-url = "https://docs.google.com/spreadsheets/d/1zxn_8nitemCcFP5D-pGMRHwt6UYowfWnPx_Mq3gXVo4/edit#gid=0"
-gs2csv(url, path, fname)
+#fname = '01_studies.csv'
+#url = "https://docs.google.com/spreadsheets/d/1zxn_8nitemCcFP5D-pGMRHwt6UYowfWnPx_Mq3gXVo4/edit#gid=0"
+#gs2csv_singlesheet(url, path, fname)
 
 #measurement_info
 fname = '02_measurement_info.csv'
 url = "https://docs.google.com/spreadsheets/d/1cMvpKicY3UOhsjdb4Y5eAh6GPPy448gLVvYGLEHsUKQ/edit#gid=0"
-gs2csv(url, path, fname)
+gs2csv_singlesheet(url, path, fname)
 
 #03_temperatures
 fname = '03_temperatures.csv'
 url =  "https://docs.google.com/spreadsheets/d/1eB7Al8YUfPR3EYg2ETKw4QKbDFvdGy8W08daHsb94yg/edit#gid=0"
-gs2csv(url, path, fname)
+gs2csv_singlesheet(url, path, fname)
