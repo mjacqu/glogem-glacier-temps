f81b8eb46a7856df37823e5e0f6479afdc259d99
Mylene Jacquemart <mjacqu@gmail.com>
2022-11-10 10:04:21 +0100

    removed unnecessary merge, added stats

--- assess_digitization_errors.py
+++ assess_digitization_errors.py
@@ -6,29 +6,34 @@ import GloGlaThelpers as ggthelp
 from scipy import interpolate
 
 
-path = '/Users/mistral/git_repos/GloGlaT'
+path = '/Users/mistral/git_repos/GloGlaT/'
 
-orig_dig = pd.read_csv(os.path.join(path, 'temperatures.csv'),
+orig_dig = pd.read_csv(os.path.join(path, 'data.csv'),
     usecols=['study_id', 'measurement_id', 'temperature_degC', 'depth_m'])
 
-re_dig = temps = pd.read_csv(os.path.join(path, 'redigitized_temps.csv'),
+re_dig = pd.read_csv(os.path.join(path, 'redigitized_temps.csv'),
     usecols=['study_id', 'measurement_id', 'temperature_degC', 'depth_m'])
-
-check_dig = pd.merge(orig_dig, re_dig, on=['study_id', 'measurement_id'], how='inner')
 
 diffs = []
-for i in set(zip(check_dig.study_id, check_dig.measurement_id)):
+ids = []
+for i in set(zip(re_dig.study_id, re_dig.measurement_id)):
     try:
         f = interpolate.interp1d(orig_dig[(orig_dig.study_id==i[0]) & (orig_dig.measurement_id==i[1])].depth_m, orig_dig[(orig_dig.study_id==i[0]) & (orig_dig.measurement_id==i[1])].temperature_degC, bounds_error=False)
     except ValueError:
         print(f"{i[0]}_{i[1]} doesn't have enough values, skipping")
-    T_interp = f(re_dig[(re_dig.study_id==i[0]) & (re_dig.measurement_id==i[1])].depth_m)
-    diff = T_interp - re_dig[(re_dig.study_id==i[0]) & (re_dig.measurement_id==i[1])].temperature_degC
-    diffs.append(diff)
+    else:
+        T_interp = f(re_dig[(re_dig.study_id==i[0]) & (re_dig.measurement_id==i[1])].depth_m)
+        diff = T_interp - re_dig[(re_dig.study_id==i[0]) & (re_dig.measurement_id==i[1])].temperature_degC
+        ids.append(i)
+        diffs.append(diff)
 
+#mean differences
+med_diff = [np.nanmedian(d) for d in diffs]
+med_diff_dict = {ids[i]: med_diff[i] for i in range(len(ids))}
+print(f"Mean difference of digitizations: {np.nanmean(med_diff)}")
+print(f"Max deviation of digitizations: {np.nanmax(np.abs(med_diff))}")
 
-
-for i in set(zip(check_dig.study_id, check_dig.measurement_id)):
+for i in set(zip(re_dig.study_id, re_dig.measurement_id)):
     f, ax = plt.subplots()
     d_orig = orig_dig[(orig_dig.study_id==i[0]) & (orig_dig.measurement_id==i[1])].depth_m
     t_orig = orig_dig[(orig_dig.study_id==i[0]) & (orig_dig.measurement_id==i[1])].temperature_degC
@@ -40,7 +45,7 @@ for i in set(zip(check_dig.study_id, check_dig.measurement_id)):
     ax.set_xlabel('Temperature (Deg. C)')
     ax.invert_yaxis()
     ax.legend()
-    f.savefig(f"digitization_assessment/{i[0]}_{i[1]}.png")
+    f.savefig(f"digitization_assessment_02/{np.int(i[0])}_{np.int(i[1])}.png")
 
 plt.close('all')
 
@@ -50,15 +55,7 @@ ax.hist([d.median() for d in diffs], bins=100)
 ax.set_title('Digitization differences')
 ax.set_xlabel('Temperature (°C)')
 ax.set_ylabel('Number of digitizations')
-f.savefig('digitization_assessment/hist_of_diffs.png')
-
-f, ax = plt.subplots()
-ax.hist([d.median() for d in diffs], bins=100)
-ax.set_title('Digitization differences')
-ax.set_xlabel('Temperature (°C)')
-ax.set_ylabel('Number of digitizations')
-ax.set_xlim([-2,2])
-f.savefig('digitization_assessment/zoom_hist_of_diffs.png')
+f.savefig('digitization_assessment_02/hist_of_diffs.png')
 
 
 ids = list(set(zip(check_dig.study_id, check_dig.measurement_id)))
